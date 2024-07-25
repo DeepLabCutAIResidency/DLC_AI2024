ef6a055968e54b8a793d297fb35c195bfea3e352
Marc Canela <129739120+marccanela@users.noreply.github.com>
2024-07-25 15:10:12 +0200

    Turning code into functions

--- analysis_pipeline/task_3.py
+++ analysis_pipeline/task_3.py
@@ -3,131 +3,128 @@ Created on Tue Jul 23 14:05:15 2024
 @author: mcanela
 """
 
-import json
-
 import matplotlib.pyplot as plt
 import numpy as np
-
-folder = "//folder/becell/Lab Projects/ERCstG_HighMemory/Data/Marc/DLC Residency 2024/bird_files/cowbird"
-
-folder_tag = folder.split("/")[-1]
-train_file = f"{folder}/annotations/train.json"
-test_file = f"{folder}/annotations/test.json"
-files = [train_file, test_file]
 
 # =============================================================================
 # Identify unusual boxes
 # =============================================================================
 
-bbox_widths = []
-bbox_heights = []
-bbox_areas = []
-bbox_aspect_ratios = []
+def calculate_width_height(data): 
+    bbox_widths = []
+    bbox_heights = []   
+    annotations = data["annotations"]
+    for ann in annotations:
+        if "bbox" in ann:
+            bbox = ann["bbox"]
+            width, height = bbox[2], bbox[3]
+            bbox_widths.append(width)
+            bbox_heights.append(height)
+    return bbox_widths, bbox_heights
 
-for file in files:
-    with open(file, "r") as f:
-        coco_data = json.load(f)
-        images = coco_data["images"]
-        annotations = coco_data["annotations"]
-        categories = coco_data["categories"]
 
-        for ann in annotations:
-            if "bbox" in ann:
-                bbox = ann["bbox"]
-                width, height = bbox[2], bbox[3]
-                area = width * height
-                aspect_ratio = width / height if height > 0 else 0
+def calculate_area(bbox_widths, bbox_heights):
+    bbox_areas = []
+    for width, height in zip(bbox_widths, bbox_heights):
+        area = width * height
+        bbox_areas.append(area)
+    return bbox_areas
 
-                bbox_widths.append(width)
-                bbox_heights.append(height)
-                bbox_areas.append(area)
-                bbox_aspect_ratios.append(aspect_ratio)
 
-fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(18, 5))
-fig.suptitle(folder_tag)  # Set the title for the entire figure
+def calculate_ratio(bbox_widths, bbox_heights):
+    bbox_aspect_ratios = []
+    for width, height in zip(bbox_widths, bbox_heights):
+        aspect_ratio = width / height if height > 0 else 0
+        bbox_aspect_ratios.append(aspect_ratio)
+    return bbox_aspect_ratios
 
-ax1.scatter(bbox_widths, bbox_heights)
-ax1.set_title('')
-ax1.set_xlabel('Width')
-ax1.set_ylabel('Height')
 
-ax2.hist(bbox_areas, bins=50, log=False)
-ax2.set_title("")
-ax2.set_xlabel("Area (width * height)")
-ax2.set_ylabel("Frequency")
+def plot_width_height(bbox_widths, bbox_heights, ax=None): 
+    if ax is None:
+        fig, ax = plt.subplots(figsize=(6, 5))
+    ax.scatter(bbox_widths, bbox_heights)
+    ax.set_title('')
+    ax.set_xlabel('Width')
+    ax.set_ylabel('Height')
+    return ax
 
-ax3.hist(bbox_aspect_ratios, bins=50, log=False)
-ax3.set_title("")
-ax3.set_xlabel("Ratio (width / height)")
-ax3.set_ylabel("Frequency")
 
-plt.tight_layout()
-plt.show()
+def plot_area(bbox_areas, ax=None): 
+    if ax is None:
+        fig, ax = plt.subplots(figsize=(6, 5))
+    ax.hist(bbox_areas, bins=50, log=False)
+    ax.set_title("")
+    ax.set_xlabel("Area (width * height)")
+    ax.set_ylabel("Frequency")
+    return ax       
+                
+                
+def plot_ratio(bbox_aspect_ratios, ax=None): 
+    if ax is None:
+        fig, ax = plt.subplots(figsize=(6, 5))
+    ax.hist(bbox_aspect_ratios, bins=50, log=False)
+    ax.set_title("")
+    ax.set_xlabel("Ratio (width / height)")
+    ax.set_ylabel("Frequency")
+    return ax
 
 # =============================================================================
 # Oculded and non-occluded keypoints
 # =============================================================================
 
-keypoints_total = 0
-keypoints_unlabeled = 0
-keypoints_labeled_occluded = 0
-keypoint_labeled_visible = 0
+def compute_keypoints(data):
+    keypoints_total = 0
+    keypoints_unlabeled = 0
+    keypoints_labeled_occluded = 0
+    keypoint_labeled_visible = 0
+    annotations = data["annotations"]
+    for ann in annotations:
+        if "keypoints" in ann:
+            keypoints = ann["keypoints"]
+            for i in range(0, len(keypoints), 3):
+                visibility = keypoints[i + 2]
+                keypoints_total += 1
+                if visibility == 0:
+                    keypoints_unlabeled += 1
+                elif visibility == 1:
+                    keypoints_labeled_occluded += 1
+                elif visibility == 2:
+                    keypoint_labeled_visible += 1
+    percent_visible = keypoint_labeled_visible / keypoints_total * 100
+    percent_occluded = keypoints_labeled_occluded / keypoints_total * 100
+    percent_unlabeled = keypoints_unlabeled / keypoints_total * 100
+    return percent_visible, percent_occluded, percent_unlabeled
 
-for file in files:
-    with open(file, "r") as f:
-        coco_data = json.load(f)
-        images = coco_data["images"]
-        annotations = coco_data["annotations"]
-        categories = coco_data["categories"]
 
-        for ann in annotations:
-            if "keypoints" in ann:
-                keypoints = ann["keypoints"]
-                for i in range(0, len(keypoints), 3):
-                    visibility = keypoints[i + 2]
-                    keypoints_total += 1
-                    if visibility == 0:
-                        keypoints_unlabeled += 1
-                    elif visibility == 1:
-                        keypoints_labeled_occluded += 1
-                    elif visibility == 2:
-                        keypoint_labeled_visible += 1
+def plot_keypoint_percent(percent_visible, percent_occluded, percent_unlabeled, ax=None): 
+    if ax is None:
+        fig, ax = plt.subplots(figsize=(8, 2))
+    percentages = [percent_visible, percent_occluded, percent_unlabeled]
+    labels = ["Visible (v=2)", "Occluded (v=1)", "Unlabeled (v=0)"]
+    colors = ["green", "orange", "red"]
+    ax.barh(["Keypoints Visibility"], [percent_visible], color=colors[0], label=labels[0])
+    ax.barh(
+        ["Keypoints Visibility"],
+        [percent_occluded],
+        left=[percent_visible],
+        color=colors[1],
+        label=labels[1],
+    )
+    ax.barh(
+        ["Keypoints Visibility"],
+        [percent_unlabeled],
+        left=[percent_visible + percent_occluded],
+        color=colors[2],
+        label=labels[2],
+    )
+    ax.gca().spines["top"].set_visible(False)
+    ax.gca().spines["right"].set_visible(False)
+    ax.gca().spines["left"].set_visible(False)
+    ax.gca().spines["bottom"].set_visible(False)
+    ax.gca().get_yaxis().set_visible(False)
+    ax.xlabel("% of Keypoints")
+    ax.legend(loc="upper right")
+    ax.xlim(0, 100)
+    plt.tight_layout()
+    return ax
 
-percent_visible = keypoint_labeled_visible / keypoints_total * 100
-percent_occluded = keypoints_labeled_occluded / keypoints_total * 100
-percent_unlabeled = keypoints_unlabeled / keypoints_total * 100
-
-percentages = [percent_visible, percent_occluded, percent_unlabeled]
-labels = ["Visible (v=2)", "Occluded (v=1)", "Unlabeled (v=0)"]
-colors = ["green", "orange", "red"]
-
-plt.figure(figsize=(8, 2))
-plt.title(folder_tag)
-
-plt.barh(["Keypoints Visibility"], [percent_visible], color=colors[0], label=labels[0])
-plt.barh(
-    ["Keypoints Visibility"],
-    [percent_occluded],
-    left=[percent_visible],
-    color=colors[1],
-    label=labels[1],
-)
-plt.barh(
-    ["Keypoints Visibility"],
-    [percent_unlabeled],
-    left=[percent_visible + percent_occluded],
-    color=colors[2],
-    label=labels[2],
-)
-
-plt.gca().spines["top"].set_visible(False)
-plt.gca().spines["right"].set_visible(False)
-plt.gca().spines["left"].set_visible(False)
-plt.gca().spines["bottom"].set_visible(False)
-plt.gca().get_yaxis().set_visible(False)
-
-plt.xlabel("% of Keypoints")
-plt.legend(loc="upper right")
-plt.xlim(0, 100)
-plt.tight_layout()
-plt.show()
