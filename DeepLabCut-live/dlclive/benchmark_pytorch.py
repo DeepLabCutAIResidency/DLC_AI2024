b3efa959127a8a0c3c23e7f66d2f9c563c090590
AnnaStuckert <47814177+AnnaStuckert@users.noreply.github.com>
2024-08-22 21:50:46 +0200

    add function for obtaining system settings for benchmarking

--- DeepLabCut-live/dlclive/benchmark_pytorch.py
+++ DeepLabCut-live/dlclive/benchmark_pytorch.py
@@ -11,6 +11,120 @@ from PIL import ImageColor
 from dlclive import DLCLive
 
 
+def download_benchmarking_data(
+    target_dir=".",
+    url="http://deeplabcut.rowland.harvard.edu/datasets/dlclivebenchmark.tar.gz",
+):
+    """
+    Downloads a DeepLabCut-Live benchmarking Data (videos & DLC models).
+    """
+    import tarfile
+    import urllib.request
+
+    from tqdm import tqdm
+
+    def show_progress(count, block_size, total_size):
+        pbar.update(block_size)
+
+    def tarfilenamecutting(tarf):
+        """' auxfun to extract folder path
+        ie. /xyz-trainsetxyshufflez/
+        """
+        for memberid, member in enumerate(tarf.getmembers()):
+            if memberid == 0:
+                parent = str(member.path)
+                l = len(parent) + 1
+            if member.path.startswith(parent):
+                member.path = member.path[l:]
+                yield member
+
+    response = urllib.request.urlopen(url)
+    print(
+        "Downloading the benchmarking data from the DeepLabCut server @Harvard -> Go Crimson!!! {}....".format(
+            url
+        )
+    )
+    total_size = int(response.getheader("Content-Length"))
+    pbar = tqdm(unit="B", total=total_size, position=0)
+    filename, _ = urllib.request.urlretrieve(url, reporthook=show_progress)
+    with tarfile.open(filename, mode="r:gz") as tar:
+        tar.extractall(target_dir, members=tarfilenamecutting(tar))
+
+
+import os
+import platform
+import subprocess
+import sys
+
+import torch
+from pip._internal.operations import freeze
+
+from dlclive import VERSION
+
+
+def get_system_info() -> dict:
+    """Return summary info for system running benchmark.
+
+    Returns
+    -------
+    dict
+        Dictionary containing the following system information:
+        * ``host_name`` (str): name of machine
+        * ``op_sys`` (str): operating system
+        * ``python`` (str): path to python (which conda/virtual environment)
+        * ``device`` (tuple): (device type (``'GPU'`` or ``'CPU'```), device information)
+        * ``freeze`` (list): list of installed packages and versions
+        * ``python_version`` (str): python version
+        * ``git_hash`` (str, None): If installed from git repository, hash of HEAD commit
+        * ``dlclive_version`` (str): dlclive version from :data:`dlclive.VERSION`
+    """
+
+    # Get OS and host name
+    op_sys = platform.platform()
+    host_name = platform.node().replace(" ", "")
+
+    # Get Python executable path
+    if platform.system() == "Windows":
+        host_python = sys.executable.split(os.path.sep)[-2]
+    else:
+        host_python = sys.executable.split(os.path.sep)[-3]
+
+    # Try to get git hash if possible
+    git_hash = None
+    dlc_basedir = os.path.dirname(os.path.dirname(__file__))
+    try:
+        git_hash = (
+            subprocess.check_output(["git", "rev-parse", "HEAD"], cwd=dlc_basedir)
+            .decode("utf-8")
+            .strip()
+        )
+    except subprocess.CalledProcessError:
+        # Not installed from git repo, e.g., pypi
+        pass
+
+    # Get device info (GPU or CPU)
+    if torch.cuda.is_available():
+        dev_type = "GPU"
+        dev = [torch.cuda.get_device_name(torch.cuda.current_device())]
+    else:
+        from cpuinfo import get_cpu_info
+
+        dev_type = "CPU"
+        dev = [get_cpu_info()["brand_raw"]]
+
+    return {
+        "host_name": host_name,
+        "op_sys": op_sys,
+        "python": host_python,
+        "device_type": dev_type,
+        "device": dev,
+        "freeze": list(freeze.freeze()),
+        "python_version": sys.version,
+        "git_hash": git_hash,
+        "dlclive_version": VERSION,
+    }
+
+
 def analyze_video(
     video_path: str,
     model_path: str,
