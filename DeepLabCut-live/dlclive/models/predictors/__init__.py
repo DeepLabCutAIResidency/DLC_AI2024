598e3fce110919bc79f11a30f9521fc1fd442863
Niels Poulsen <niels.poulsen@epfl.ch>
2025-02-17 14:17:40 +0100

    add PAF predictor

--- DeepLabCut-live/dlclive/models/predictors/__init__.py
+++ DeepLabCut-live/dlclive/models/predictors/__init__.py
@@ -1,10 +1,14 @@
-"""
-DeepLabCut Toolbox (deeplabcut.org)
-© A. & M. Mathis Labs
-
-Licensed under GNU Lesser General Public License v3.0
-"""
-
+#
+# DeepLabCut Toolbox (deeplabcut.org)
+# © A. & M.W. Mathis Labs
+# https://github.com/DeepLabCut/DeepLabCut
+#
+# Please see AUTHORS for contributors.
+# https://github.com/DeepLabCut/DeepLabCut/blob/main/AUTHORS
+#
+# Licensed under GNU Lesser General Public License v3.0
+#
 from dlclive.models.predictors.base import PREDICTORS, BasePredictor
 from dlclive.models.predictors.dekr_predictor import DEKRPredictor
 from dlclive.models.predictors.single_predictor import HeatmapPredictor
+from dlclive.models.predictors.paf_predictor import PartAffinityFieldPredictor
